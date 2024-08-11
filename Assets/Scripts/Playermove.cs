85670888ab3ec5e179d058c88a86935c7a7c5a34
pipiPAPICO <163659664+pipiPAPICO@users.noreply.github.com>
2024-08-11 15:51:31 +0900

    Player
    laycast

--- Assets/Scripts/Playermove.cs
+++ Assets/Scripts/Playermove.cs
@@ -7,41 +7,65 @@ using UnityEngine.Rendering;
 
 public class Playermove : MonoBehaviour
 {
-    Rigidbody2D rb;
 
-    public float speed = 10;
-    public float jumpPower = 10;
-
-
+    public float maxSpeed;
+    public float jumpPower;
+    Rigidbody2D rigid;
+    SpriteRenderer spriteRenderer;
+    Animator anim;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.A))
+        //Jump
+        if (Input.GetButtonDown("Jump") && !anim.GetBool("isJumping"))
         {
-            rb.velocity = new Vector2(-speed, rb.velocity.y);
-            transform.localScale = new Vector2(-5, 5);
+            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            anim.SetBool("isJumping", true);
         }
-        else if (Input.GetKey(KeyCode.D))
+        //Speed
+        if (Input.GetButtonUp("Horizontal"))
         {
-            rb.velocity = new Vector2(speed, rb.velocity.y);
-            transform.localScale = new Vector3(5, 5);
+            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f,rigid.velocity.y);
 
         }
-       if (Input.GetKeyDown(KeyCode.Space))
-        {
-            rb.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
-        }
+        //방향 전환
+        if (Input.GetButton("Horizontal"))
+            spriteRenderer.flipX = Input.GetAxisRaw ("Horizontal") == -1;
+
+        if (Mathf.Abs(rigid.velocity.x) < 0.3)
+            anim.SetBool("isrunning", false);
+        else
+            anim.SetBool("isrunning", true);
     }
-    bool IsGrounded()
+    void FixedUpdate()
     {
-        // 캐릭터를 중심으로 아래 방향으로 ray 를 쏘아 그 곳에 Layer 타입이 Ground 인 객체가 있는지 검사합니다.
-        var ray = Physics2D.Raycast(transform.position, Vector2.down, 1f, 1 << LayerMask.NameToLayer("Ground"));
-        return ray.collider != null;
+        float h = Input.GetAxisRaw("Horizontal");
+        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+
+        if (rigid.velocity.x > maxSpeed)
+            rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
+        else if (rigid.velocity.x < maxSpeed * (-1))
+            rigid.velocity = new Vector2(maxSpeed * (-1), rigid.velocity.y);
+
+        if(rigid.velocity.y < 0)
+        {
+            Debug.DrawRay(rigid.position, Vector3.down, new Color(0, 1, 0));
+
+            RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1, LayerMask.GetMask("Platform"));
+
+            if (rayHit.collider != null)
+            {
+                if (rayHit.distance < 0.5f)
+                    anim.SetBool("isJumping", false);
+            }
+        }
     }
 }
