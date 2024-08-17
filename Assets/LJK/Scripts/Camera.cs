00ce63735e02929d47d4f186033965956a66b4c8
Frogrres <jnggmi@pukyong.ac.kr>
2024-08-17 14:45:32 +0900

    test
    점프버그존재, 플레이어 속도 및 점프파워 조정필요, 엔딩구현 필요

--- Assets/LJK/Scripts/Camera.cs
+++ Assets/LJK/Scripts/Camera.cs
@@ -21,6 +21,6 @@ public class Camera : MonoBehaviour
 
     void LateUpdate()
     {
-        cam_transform.position = new Vector3(player_tr.transform.position.x + 7, 1, -15);
+        cam_transform.position = new Vector3(player_tr.transform.position.x + 7, 1, -10);
     }
 }
